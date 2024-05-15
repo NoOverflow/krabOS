984d80b5437b44ade18adf80a609cabaacdddcb3
NoOverflow <julien.prost31@ynov.com>
2024-05-15 20:14:05 +0200

    feat: add print string helper function

---
+++ project/bootloader/utils/real/print_string.asm
@@ -0,0 +1,34 @@
+;
+; KrabbOS utils -- disk_read
+;
+; This file is part of KrabbOS.
+;
+; Print a string to the screen in TTY mode TODO: Use 0x10 AH 0x13H
+;
+; Inputs:
+;   si = pointer to string
+print_string:
+    pusha
+    mov ah, 0x0E ; Enable BIOS TTY
+    mov cx, 0    ; Start at 0
+    .loop:
+        lodsb ; Load next byte from string
+        test al, al ; Did we reach 0 byte (end of string)
+        jz .done
+        int 0x10 ; Print character
+        jmp .loop
+
+    .done:
+        popa
+        ret
+
+print_string_nl:
+    pusha
+    call print_string
+    mov ah, 0x0E
+    mov al, 0x0A
+    int 0x10
+    mov al, 0x0D
+    int 0x10
+    popa
+    ret
