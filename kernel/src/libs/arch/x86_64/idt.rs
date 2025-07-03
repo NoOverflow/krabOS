d589e67d2792674b8906bea6631c4b67dc3dcd8a
NoOverflow <julien.prost31@ynov.com>
2025-07-04 01:13:53 +0200

    feat: add idt struct

---
+++ kernel/src/libs/arch/x86_64/idt.rs
@@ -0,0 +1,106 @@
+use core::arch::asm;
+
+use crate::libs::arch::x86_64::gdt::SegmentSelector;
+
+#[repr(u8)]
+pub enum IdtGateType {
+    Interrupt = 0xE,
+    Trap = 0xF,
+}
+
+struct IdtGateDescriptorProperties {
+    gate_type: IdtGateType,
+    privilege_level: u8,
+}
+
+#[repr(C, packed)]
+#[derive(Default)]
+struct IdtGateDescriptor {
+    pub ep_ll: u16,
+    pub segment_selector: u16,
+    pub ist_offset: u8,
+    pub properties: u8,
+    pub ep_lh: u16,
+    pub ep_hh: u32,
+    _reserved: u32,
+}
+
+pub type Idt = [IdtGateDescriptor; 256];
+
+#[repr(C, packed)]
+struct IdtDescriptor {
+    pub size: u16,
+    pub idt_offset: *const Idt,
+}
+
+impl Into<u8> for IdtGateDescriptorProperties {
+    fn into(self) -> u8 {
+        let mut ret: u8 = 0;
+
+        ret |= self.gate_type as u8;
+        ret |= self.privilege_level << 5;
+        ret |= 1 << 7;
+
+        ret
+    }
+}
+
+impl IdtGateDescriptor {
+    pub fn new(
+        entry_point: u64,
+        segment_selector: SegmentSelector,
+        properties: IdtGateDescriptorProperties,
+        ist_offset: u8,
+    ) -> Self {
+        let mut ret = IdtGateDescriptor::default();
+
+        ret.ep_ll = entry_point as u16;
+        ret.segment_selector = segment_selector.into();
+        ret.ist_offset = ist_offset & 0x7;
+        ret.properties = properties.into();
+        ret.ep_lh = (entry_point >> 16) as u16;
+        ret.ep_hh = (entry_point >> 32) as u32;
+
+        ret
+    }
+}
+
+pub fn load(idtr: &Idt) {
+    unsafe {
+        asm!(
+            "cli",
+            "lidt [{idtr}]",
+            idtr = in(reg) idtr
+        );
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::libs::arch::x86_64::{
+        gdt::{CPL_RING_3, SegmentSelector},
+        idt::{IdtGateDescriptor, IdtGateDescriptorProperties, IdtGateType},
+    };
+
+    #[test]
+    fn idt_test_serialize() {
+        let segsel = SegmentSelector {
+            global_descriptor_table: true,
+            index: 0x546,
+            requested_privilege: CPL_RING_3,
+        };
+        let properties = IdtGateDescriptorProperties {
+            privilege_level: CPL_RING_3,
+            gate_type: IdtGateType::Interrupt,
+        };
+        let idtgdesc = IdtGateDescriptor::new(0xF8D99A8B66647936, segsel, properties, 0b110);
+        let ll = idtgdesc.ep_ll;
+        let hh = idtgdesc.ep_hh;
+        let lh = idtgdesc.ep_lh;
+
+        assert_eq!(ll, 0x7936);
+        assert_eq!(hh, 0xF8D99A8B);
+        assert_eq!(lh, 0x6664);
+        assert_eq!(size_of::<IdtGateDescriptor>() * 8, 128);
+    }
+}
